60cd19c501d7916545b8543a9129d25ff455a444
Alexey Zrelov <azheimdall@gmail.com>
2022-02-25 18:38:36 +0200

    update

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\View;
+use App\Models\Dbh;
+use PDO;
 
 class UsersController
 {
@@ -10,20 +12,33 @@ class UsersController
     {
         // get information from database
         // create array with Article objects
+        $stmt = (new Dbh())->connect()->prepare('SELECT * FROM article');
+        $stmt->execute();
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//        echo "<pre>";
+//        print_r($res);
 
         return new View('Users/index.html', [
-            'articles' => []
+            'articles' => $res
         ]);
-
     }
 
     public function show(array $vars): View
     {
         // $vars['id'];
         // get information from database where article ID = $vars['id']
+        $stmt = (new Dbh())->connect()->prepare('SELECT * FROM article WHERE id = ?');
+        $stmt->execute([$vars['id']]);
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//        echo "<pre>";
+//        var_dump($res[0]);
 
         return new View('Users/show.html', [
-            'id' => $vars['id']
+            'id' => $res[0]['id'],
+            'title' => $res[0]['title'],
+            'descriptions' => $res[0]['descriptions']
         ]);
     }
 }
